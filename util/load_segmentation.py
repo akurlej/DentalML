f02949c60d24b0487aaf555213ae7f01f492df87
Arthur Kurlej <akurlej@gmail.com>
2022-03-26 13:53:24 -0400

    added flipud/rotations/saltnpepper/translations

--- util/load_segmentation.py
+++ util/load_segmentation.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import tensorflow_addons as tfa
 from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,27 +35,64 @@ def parse_image(img_shape: list, img: str):
     msk = tf.cast(msk, tf.uint8)
     return (img,msk)
 
-def augment_image(img,mask,seed,enable):
+def augment_image(img,mask,enable):
   #from https://dergipark.org.tr/tr/download/article-file/1817118
   #horizontal flip resulted increased performance
   if enable[0]:
-    do_flip = tf.random.uniform([],seed=seed) > 0.5
+    do_flip = tf.random.uniform([]) > 0.5
     img = tf.cond(do_flip, lambda: tf.image.flip_left_right(img), lambda: img)
     mask = tf.cond(do_flip, lambda: tf.image.flip_left_right(mask), lambda: mask)
   #image brightness
   if enable[1]:
     img = tf.image.adjust_brightness(img, \
             delta = tf.random.uniform([], minval=-0.25, maxval=0.25,\
-                                     dtype=tf.dtypes.float32,seed=seed))
+                                     dtype=tf.dtypes.float32))
+  
+  #vertical flip
+  if enable[2]:
+    do_flip = tf.random.uniform([]) > 0.5
+    img = tf.cond(do_flip, lambda: tf.image.flip_up_down(img), lambda: img)
+    mask = tf.cond(do_flip, lambda: tf.image.flip_up_down(mask), lambda: mask)
+  #rotations
+  if enable[3]:
+    #get a random degree
+    angle = tf.random.uniform([],minval=-20*np.pi/180,maxval=20*np.pi/180)
+    img = tfa.image.rotate(images=img,angles=angle,fill_mode='constant',\
+                            fill_value = 0)
+    mask = tfa.image.rotate(images=mask,angles=angle,fill_mode='constant',\
+                            fill_value = 0)
+  #translations
+  if enable[4]:
+    dx = int(tf.random.uniform(
+        [], minval=-int(0.2*img.shape[0]), maxval=+int(0.2*img.shape[0])))
+    dy = int(tf.random.uniform([], minval=-int(0.2*mask.shape[1]),
+             maxval=+int(0.2*mask.shape[1])))
+    img = tfa.image.translate(images=img,translations = [dx,dy], fill_value = 0)
+    mask = tfa.image.translate(images=mask,translations = [dx,dy], fill_value = 0)
+
+  #salt n peppa
+  if enable[5]:
+    #lets figure out we can do 20% of all pixels
+    random_values = tf.random.uniform(shape=img.shape)
+    prob_salt = 0.05
+    prob_pepper = 0.95
+    img = tf.where(random_values < prob_salt, np.dtype('uint8').type(0), img)
+    img = tf.where(random_values > prob_pepper, np.dtype('uint8').type(255), img)
+
+  #image blurring, how does this work with the mask? should the mask be blurred as well?
 
   return (img,mask)
 
 
-def generate_dataset(dataPath, imageType, seed=42, img_shape=(512,512,1), batch_size = 16,enable_augmentation = (1,1),\
-                     repeat_count=3, kfold = 1):
+def generate_dataset(dataPath, imageType, seed=42, img_shape=(512,512,1), batch_size = 16,\
+                      enable_augmentation = (1,1,1,1,1,1), repeat_count=3, kfold = 1):
     #list
+    tf.random.set_seed(seed=seed) #set the seed.
     datasets = []
     img_shape = [img_shape[0], img_shape[1]]
+
+    if len(enable_agumentation) != 6:
+      raise ValueError("Enable augmentation value not of appropriate length")
 
     if not any(enable_augmentation):
       print("no image augmentaton")
@@ -67,7 +105,7 @@ def generate_dataset(dataPath, imageType, seed=42, img_shape=(512,512,1), batch_
       train_dataset = files.shard(num_shards = kfold, index = k)
       train_dataset = train_dataset.shuffle(seed=seed, buffer_size = len(train_dataset), reshuffle_each_iteration=True)
       train_dataset = train_dataset.map(lambda x: parse_image(img_shape,x),num_parallel_calls=tf.data.AUTOTUNE)
-      train_dataset = train_dataset.map(lambda x,y: augment_image(x,y,seed,enable_augmentation),\
+      train_dataset = train_dataset.map(lambda x,y: augment_image(x,y,enable_augmentation),\
                       num_parallel_calls=tf.data.AUTOTUNE)
       if batch_size is not None:
         train_dataset = train_dataset.batch(batch_size)
